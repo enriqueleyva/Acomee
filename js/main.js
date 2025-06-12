cdb0860e0e0bc58b83ed9032c9a03a53fa1b7fc6
Enrique Lopez <enrique.leyva.124@gmail.com>
2025-06-12 01:01:36 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,380 @@
+const _0x2c2de3 = _0x45b6;
+(function (_0x2064a3, _0x3e9aa6) {
+	const _0xb493d8 = _0x45b6,
+		_0x191015 = _0x2064a3();
+	while (!![]) {
+		try {
+			const _0x293093 =
+				(parseInt(_0xb493d8(0xc8)) / 0x1) * (-parseInt(_0xb493d8(0xc4)) / 0x2) +
+				-parseInt(_0xb493d8(0xf0)) / 0x3 +
+				parseInt(_0xb493d8(0x102)) / 0x4 +
+				-parseInt(_0xb493d8(0x100)) / 0x5 +
+				(-parseInt(_0xb493d8(0xf3)) / 0x6) *
+					(-parseInt(_0xb493d8(0xc2)) / 0x7) +
+				(parseInt(_0xb493d8(0xd6)) / 0x8) * (-parseInt(_0xb493d8(0xc5)) / 0x9) +
+				parseInt(_0xb493d8(0xdf)) / 0xa;
+			if (_0x293093 === _0x3e9aa6) break;
+			else _0x191015["push"](_0x191015["shift"]());
+		} catch (_0x4c9e6a) {
+			_0x191015["push"](_0x191015["shift"]());
+		}
+	}
+})(_0x4e07, 0x190e0);
+let allProducts = [],
+	filteredProducts = [];
+const itemsPerPage = 0x9;
+function _0x4e07() {
+	const _0x5acd57 = [
+		"\x22\x20class=\x22btn\x20btn-primary\x22\x20target=\x22_blank\x22\x20rel=\x22noopener\x20noreferrer\x22>Ver\x20Producto</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+		"20652eGCIgj",
+		"9AqggWf",
+		"POST",
+		"slice",
+		"8aVVPgl",
+		"toFixed",
+		"parse",
+		"createElement",
+		"active-filter",
+		"style",
+		"trim",
+		"disabled",
+		"\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22mt-auto\x20d-flex\x20justify-content-between\x20align-items-center\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22card-price\x22>$",
+		"map",
+		"toUpperCase",
+		"image_link_s",
+		"price",
+		"target",
+		"517112lPZayb",
+		"Respuesta\x20de\x20la\x20API:",
+		"querySelectorAll",
+		"href",
+		"error",
+		"filter",
+		"block",
+		"none",
+		"<a\x20class=\x22page-link\x22\x20href=\x22#\x22\x20data-page=\x22",
+		"2445870GZBuLc",
+		"mode",
+		"log",
+		"div",
+		"loader",
+		"addEventListener",
+		"decrypt",
+		"12345678901234567890123456789012",
+		"mb-4",
+		"add",
+		"pagination-controls",
+		"Error\x20al\x20obtener\x20datos\x20de\x20la\x20API:",
+		"<div\x20class=\x22col-12\x22><p\x20class=\x22text-danger\x22>Estructura\x20de\x20datos\x20inesperada\x20de\x20la\x20API.</p></div>",
+		"display",
+		"\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22card\x20h-100\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22",
+		"</span>",
+		"</h5>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+		"597015vpMOmA",
+		"remove",
+		"category",
+		"1127298xnnJNW",
+		"<div\x20class=\x22col-12\x22><p\x20class=\x22text-danger\x22>No\x20se\x20pudieron\x20cargar\x20los\x20datos\x20de\x20la\x20API.\x20Inténtalo\x20de\x20nuevo\x20más\x20tarde.</p></div>",
+		"<div\x20class=\x22col-12\x22><p\x20class=\x22text-danger\x22>Error\x20al\x20procesar\x20los\x20datos.</p></div>",
+		"value",
+		"ceil",
+		"classList",
+		"Utf8",
+		"&laquo;",
+		"length",
+		"key",
+		"page-item",
+		"Todas",
+		"CBC",
+		"553630KEIxYo",
+		"\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22card-body\x20d-flex\x20flex-column\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h5\x20class=\x22card-title\x22>",
+		"508528aKefcr",
+		"textContent",
+		"https://script.google.com/macros/s/AKfycbziLnqsOy3sACUyBx9B1783dsbQRdddf_dmg0MEFEO8gMw4qD-ooNBtBo-kf7o9c5qy/exec",
+		"forEach",
+		"&raquo;",
+		"floor",
+		"getElementById",
+		"active",
+		"search-input",
+		"product-cards-container",
+		"preventDefault",
+		"Error\x20al\x20desencriptar\x20o\x20parsear\x20el\x20JSON:",
+		"ajax",
+		"enc",
+		"\x22\x20class=\x22card-img-top\x22\x20alt=\x22",
+		"page",
+		"title",
+		"max",
+		".badge-category",
+		"fail",
+		"innerHTML",
+		"<div\x20class=\x22col-12\x20text-center\x20text-muted\x22><p>No\x20se\x20encontraron\x20productos\x20para\x20esta\x20categoría\x20o\x20búsqueda.</p></div>",
+		"toString",
+		"click",
+		"clear-search",
+		"min",
+		"respuesta",
+		"appendChild",
+		"7OeHdXV",
+	];
+	_0x4e07 = function () {
+		return _0x5acd57;
+	};
+	return _0x4e07();
+}
+let currentPage = 0x1,
+	currentCategoryFilter = _0x2c2de3(0xfe);
+const productCardsContainer = document["getElementById"](_0x2c2de3(0x10b)),
+	paginationControls = document[_0x2c2de3(0x108)](_0x2c2de3(0xe9)),
+	categoryBadgesContainer = document[_0x2c2de3(0x108)](
+		"category-badges-container"
+	),
+	searchInput = document["getElementById"](_0x2c2de3(0x10a)),
+	clearSearchIcon = document[_0x2c2de3(0x108)](_0x2c2de3(0xbe));
+function displayProducts(_0x27a07b, _0x524158) {
+	const _0x54f9e4 = _0x2c2de3;
+	productCardsContainer[_0x54f9e4(0xba)] = "";
+	const _0x4d4aa8 = (_0x524158 - 0x1) * itemsPerPage,
+		_0x2ab97b = _0x4d4aa8 + itemsPerPage,
+		_0x556e95 = _0x27a07b[_0x54f9e4(0xc7)](_0x4d4aa8, _0x2ab97b);
+	if (_0x556e95["length"] === 0x0) {
+		productCardsContainer[_0x54f9e4(0xba)] = _0x54f9e4(0xbb);
+		return;
+	}
+	_0x556e95[_0x54f9e4(0x105)]((_0x343970) => {
+		const _0x5ee37d = _0x54f9e4,
+			_0x272990 = document["createElement"](_0x5ee37d(0xe2));
+		_0x272990[_0x5ee37d(0xf8)]["add"]("col-md-4", "col-sm-6", _0x5ee37d(0xe7));
+		const _0xd095b7 = _0x343970["category"]
+				? _0x343970["category"]["trim"]()
+				: "",
+			_0x123be5 =
+				_0x343970[_0x5ee37d(0xd3)] ||
+				"https://via.placeholder.com/150/cccccc/ffffff?text=No+Image",
+			_0x550068 = _0x343970["url"] || "#";
+		(_0x272990[_0x5ee37d(0xba)] =
+			_0x5ee37d(0xed) +
+			_0x123be5 +
+			_0x5ee37d(0xb4) +
+			_0x343970[_0x5ee37d(0xb6)] +
+			_0x5ee37d(0x101) +
+			_0x343970[_0x5ee37d(0xb6)] +
+			_0x5ee37d(0xef) +
+			(_0xd095b7
+				? "<span\x20class=\x22badge\x20bg-secondary\x20mb-2\x22>" +
+				  _0xd095b7 +
+				  _0x5ee37d(0xee)
+				: "") +
+			_0x5ee37d(0xd0) +
+			parseFloat(_0x343970[_0x5ee37d(0xd4)] || 0x0)[_0x5ee37d(0xc9)](0x2) +
+			"</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22" +
+			_0x550068 +
+			_0x5ee37d(0xc3)),
+			productCardsContainer[_0x5ee37d(0xc1)](_0x272990);
+	});
+}
+function setupPagination(_0xe11b2d) {
+	const _0x3f6c65 = _0x2c2de3;
+	paginationControls[_0x3f6c65(0xba)] = "";
+	const _0x33aacb = Math[_0x3f6c65(0xf7)](_0xe11b2d / itemsPerPage),
+		_0x5e6f18 = (_0x4f66c6, _0x377c37, _0xe847c0 = ![], _0x36588c = ![]) => {
+			const _0x5bbd2a = _0x3f6c65,
+				_0x479737 = document[_0x5bbd2a(0xcb)]("li");
+			_0x479737[_0x5bbd2a(0xf8)]["add"](_0x5bbd2a(0xfd));
+			if (_0xe847c0) _0x479737["classList"][_0x5bbd2a(0xe8)]("disabled");
+			if (_0x36588c) _0x479737[_0x5bbd2a(0xf8)]["add"](_0x5bbd2a(0x109));
+			return (
+				(_0x479737[_0x5bbd2a(0xba)] =
+					_0x5bbd2a(0xde) + _0x4f66c6 + "\x22>" + _0x377c37 + "</a>"),
+				_0x479737
+			);
+		};
+	paginationControls["appendChild"](
+		_0x5e6f18(currentPage - 0x1, _0x3f6c65(0xfa), currentPage === 0x1)
+	);
+	const _0x15d9b1 = 0xa;
+	let _0x511e2c = Math[_0x3f6c65(0xb7)](
+			0x1,
+			currentPage - Math[_0x3f6c65(0x107)](_0x15d9b1 / 0x2)
+		),
+		_0x50e283 = Math[_0x3f6c65(0xbf)](_0x33aacb, _0x511e2c + _0x15d9b1 - 0x1);
+	_0x50e283 - _0x511e2c + 0x1 < _0x15d9b1 &&
+		(_0x511e2c = Math[_0x3f6c65(0xb7)](0x1, _0x50e283 - _0x15d9b1 + 0x1));
+	for (let _0xfe364e = _0x511e2c; _0xfe364e <= _0x50e283; _0xfe364e++) {
+		paginationControls[_0x3f6c65(0xc1)](
+			_0x5e6f18(_0xfe364e, _0xfe364e, ![], _0xfe364e === currentPage)
+		);
+	}
+	paginationControls[_0x3f6c65(0xc1)](
+		_0x5e6f18(currentPage + 0x1, _0x3f6c65(0x106), currentPage === _0x33aacb)
+	),
+		paginationControls["querySelectorAll"](".page-link")[_0x3f6c65(0x105)](
+			(_0x5bc684) => {
+				const _0x1697f5 = _0x3f6c65;
+				_0x5bc684["addEventListener"](_0x1697f5(0xbd), (_0x2c6f32) => {
+					const _0x2b14f5 = _0x1697f5;
+					_0x2c6f32[_0x2b14f5(0xb0)]();
+					const _0x1ababf = parseInt(
+						_0x2c6f32[_0x2b14f5(0xd5)]["dataset"][_0x2b14f5(0xb5)]
+					);
+					!isNaN(_0x1ababf) &&
+						_0x1ababf > 0x0 &&
+						_0x1ababf <= _0x33aacb &&
+						_0x1ababf !== currentPage &&
+						((currentPage = _0x1ababf),
+						displayProducts(filteredProducts, currentPage),
+						setupPagination(_0xe11b2d));
+				});
+			}
+		);
+}
+function applyCategoryFilter(_0x51839b) {
+	const _0xe77b64 = _0x2c2de3;
+	(currentCategoryFilter = _0x51839b),
+		(currentPage = 0x1),
+		_0x51839b === _0xe77b64(0xfe)
+			? (filteredProducts = allProducts)
+			: (filteredProducts = allProducts[_0xe77b64(0xdb)](
+					(_0x5e0694) =>
+						_0x5e0694[_0xe77b64(0xf2)] &&
+						_0x5e0694[_0xe77b64(0xf2)][_0xe77b64(0xce)]()[_0xe77b64(0xd2)]() ===
+							_0x51839b[_0xe77b64(0xd2)]()
+			  )),
+		displayProducts(filteredProducts, currentPage),
+		setupPagination(filteredProducts[_0xe77b64(0xfb)]),
+		updateActiveBadge(_0x51839b);
+}
+function generateCategoryBadges(_0x5d4965) {
+	const _0x40ddde = _0x2c2de3;
+	categoryBadgesContainer["innerHTML"] = "";
+	const _0x3a21d8 = new Set([_0x40ddde(0xfe), ..._0x5d4965]);
+	_0x3a21d8["forEach"]((_0x56d390) => {
+		const _0x4672d8 = _0x40ddde,
+			_0x3c3588 = document[_0x4672d8(0xcb)]("a");
+		(_0x3c3588[_0x4672d8(0xd9)] = "#"),
+			_0x3c3588[_0x4672d8(0xf8)][_0x4672d8(0xe8)]("badge-category"),
+			(_0x3c3588["textContent"] = _0x56d390),
+			_0x56d390 === currentCategoryFilter &&
+				_0x3c3588["classList"][_0x4672d8(0xe8)](_0x4672d8(0xcc)),
+			_0x3c3588[_0x4672d8(0xe4)](_0x4672d8(0xbd), (_0x3934ef) => {
+				const _0x38f418 = _0x4672d8;
+				_0x3934ef[_0x38f418(0xb0)](), applyCategoryFilter(_0x56d390);
+			}),
+			categoryBadgesContainer[_0x4672d8(0xc1)](_0x3c3588);
+	});
+}
+function updateActiveBadge(_0xa30fb) {
+	const _0x3180e8 = _0x2c2de3,
+		_0x14b755 = categoryBadgesContainer[_0x3180e8(0xd8)](_0x3180e8(0xb8));
+	_0x14b755[_0x3180e8(0x105)]((_0xbbbc75) => {
+		const _0x315d22 = _0x3180e8;
+		_0xbbbc75[_0x315d22(0x103)] === _0xa30fb
+			? _0xbbbc75[_0x315d22(0xf8)][_0x315d22(0xe8)]("active-filter")
+			: _0xbbbc75[_0x315d22(0xf8)][_0x315d22(0xf1)](_0x315d22(0xcc));
+	});
+}
+function desencriptar(_0x1da69b) {
+	const _0x31cbf1 = _0x2c2de3,
+		_0x303364 = CryptoJS["enc"][_0x31cbf1(0xf9)][_0x31cbf1(0xca)](
+			_0x31cbf1(0xe6)
+		),
+		_0xaa90a3 =
+			CryptoJS[_0x31cbf1(0xb3)][_0x31cbf1(0xf9)][_0x31cbf1(0xca)](
+				"1234567890123456"
+			);
+	console[_0x31cbf1(0xe1)](_0x1da69b);
+	const _0x4092a8 = _0x1da69b;
+	try {
+		const _0x5a5a80 = CryptoJS["AES"][_0x31cbf1(0xe5)](_0x4092a8, _0x303364, {
+				iv: _0xaa90a3,
+				mode: CryptoJS[_0x31cbf1(0xe0)][_0x31cbf1(0xff)],
+				padding: CryptoJS["pad"]["Pkcs7"],
+			}),
+			_0x588fbb = _0x5a5a80[_0x31cbf1(0xbc)](CryptoJS["enc"][_0x31cbf1(0xf9)]),
+			_0x450733 = JSON[_0x31cbf1(0xca)](_0x588fbb);
+		console[_0x31cbf1(0xe1)](_0x450733),
+			(allProducts = JSON[_0x31cbf1(0xca)](_0x450733)),
+			console[_0x31cbf1(0xe1)](allProducts, typeof allProducts),
+			(allProducts = allProducts[_0x31cbf1(0xd1)]((_0x103cc3) => ({
+				..._0x103cc3,
+				category: _0x103cc3[_0x31cbf1(0xf2)]
+					? _0x103cc3[_0x31cbf1(0xf2)][_0x31cbf1(0xce)]()
+					: "",
+			}))),
+			(filteredProducts = allProducts);
+		const _0x4a3e2a = [
+			...new Set(
+				allProducts[_0x31cbf1(0xd1)]((_0x5755ee) => _0x5755ee["category"])[
+					_0x31cbf1(0xdb)
+				](Boolean)
+			),
+		];
+		generateCategoryBadges(_0x4a3e2a),
+			applyCategoryFilter(currentCategoryFilter);
+	} catch (_0x12feda) {
+		console["error"](_0x31cbf1(0xb1), _0x12feda),
+			(productCardsContainer[_0x31cbf1(0xba)] = _0x31cbf1(0xf5));
+	}
+}
+function _0x45b6(_0x27a753, _0x99a1a4) {
+	const _0x4e07c7 = _0x4e07();
+	return (
+		(_0x45b6 = function (_0x45b6f8, _0x432e14) {
+			_0x45b6f8 = _0x45b6f8 - 0xb0;
+			let _0x594901 = _0x4e07c7[_0x45b6f8];
+			return _0x594901;
+		}),
+		_0x45b6(_0x27a753, _0x99a1a4)
+	);
+}
+function getData(_0x271548) {
+	const _0x3e4ea7 = _0x2c2de3;
+	(document[_0x3e4ea7(0x108)](_0x3e4ea7(0xe3))[_0x3e4ea7(0xcd)][
+		_0x3e4ea7(0xec)
+	] = _0x3e4ea7(0xdc)),
+		(document["getElementById"](_0x3e4ea7(0x10a))["disabled"] = !![]);
+	var _0x20e07c = {
+		url: _0x3e4ea7(0x104),
+		type: _0x3e4ea7(0xc6),
+		data: JSON["stringify"]({ texto: _0x271548 }),
+	};
+	$[_0x3e4ea7(0xb2)](_0x20e07c)
+		["done"](function (_0x3b89f4) {
+			const _0x51770d = _0x3e4ea7;
+			console[_0x51770d(0xe1)](_0x51770d(0xd7), _0x3b89f4);
+			const _0x4d19a6 = JSON[_0x51770d(0xca)](_0x3b89f4);
+			_0x4d19a6 &&
+			_0x4d19a6[_0x51770d(0xc0)] &&
+			_0x4d19a6["respuesta"][_0x51770d(0xc0)]
+				? desencriptar(_0x4d19a6[_0x51770d(0xc0)][_0x51770d(0xc0)])
+				: (console[_0x51770d(0xda)](
+						"Estructura\x20de\x20respuesta\x20inesperada:",
+						_0x3b89f4
+				  ),
+				  (productCardsContainer[_0x51770d(0xba)] = _0x51770d(0xeb))),
+				(document[_0x51770d(0x108)](_0x51770d(0xe3))["style"][_0x51770d(0xec)] =
+					_0x51770d(0xdd)),
+				(document[_0x51770d(0x108)]("search-input")[_0x51770d(0xcf)] = ![]);
+		})
+		[_0x3e4ea7(0xb9)](function (_0x519293, _0x163753, _0x9dea9e) {
+			const _0x4be171 = _0x3e4ea7;
+			(document[_0x4be171(0x108)]("loader")["style"][_0x4be171(0xec)] =
+				_0x4be171(0xdd)),
+				(document[_0x4be171(0x108)]("search-input")[_0x4be171(0xcf)] = ![]),
+				console["error"](_0x4be171(0xea), _0x163753, _0x9dea9e),
+				(productCardsContainer[_0x4be171(0xba)] = _0x4be171(0xf4));
+		});
+}
+searchInput[_0x2c2de3(0xe4)]("keyup", (_0xf34b2e) => {
+	const _0x1b1fc5 = _0x2c2de3;
+	_0xf34b2e[_0x1b1fc5(0xfc)] === "Enter" &&
+		(console[_0x1b1fc5(0xe1)](searchInput["value"]),
+		getData(searchInput[_0x1b1fc5(0xf6)]));
+}),
+	clearSearchIcon["addEventListener"](_0x2c2de3(0xbd), () => {
+		const _0x49049f = _0x2c2de3;
+		(searchInput[_0x49049f(0xf6)] = ""),
+			(currentSearchTerm = ""),
+			applyFilters();
+	});
---
+++ js/main2.js
@@ -0,0 +1,285 @@
+let allProducts = []; // Almacena todos los productos cargados
+let filteredProducts = []; // Almacena los productos después de aplicar filtros
+const itemsPerPage = 9;
+let currentPage = 1;
+let currentCategoryFilter = "Todas"; // Inicia con "Todas"
+
+const productCardsContainer = document.getElementById(
+	"product-cards-container"
+);
+const paginationControls = document.getElementById("pagination-controls");
+const categoryBadgesContainer = document.getElementById(
+	"category-badges-container"
+);
+const searchInput = document.getElementById("search-input"); // Referencia al campo de búsqueda
+const clearSearchIcon = document.getElementById("clear-search"); // Icono para limpiar búsqueda
+
+// Función para mostrar los productos en las tarjetas
+function displayProducts(productsToRender, page) {
+	productCardsContainer.innerHTML = ""; // Limpiar el contenedor
+	const startIndex = (page - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+	const productsPage = productsToRender.slice(startIndex, endIndex);
+
+	if (productsPage.length === 0) {
+		productCardsContainer.innerHTML =
+			'<div class="col-12 text-center text-muted"><p>No se encontraron productos para esta categoría o búsqueda.</p></div>';
+		return;
+	}
+
+	productsPage.forEach((producto) => {
+		const cardCol = document.createElement("div");
+		cardCol.classList.add("col-md-4", "col-sm-6", "mb-4");
+
+		// Normalizar la categoría para mostrarla (si no existe, dejar vacío)
+		const categoryDisplay = producto.category ? producto.category.trim() : "";
+		// Asegurar que image_link_s y url existan, o usar placeholders/vacíos
+		const imageUrl =
+			producto.image_link_s ||
+			"https://via.placeholder.com/150/cccccc/ffffff?text=No+Image";
+		const productUrl = producto.url || "#"; // Si no hay URL, el enlace puede no hacer nada
+
+		cardCol.innerHTML = `
+                    <div class="card h-100">
+                        <img src="${imageUrl}" class="card-img-top" alt="${
+			producto.title
+		}">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">${producto.title}</h5>
+                            ${
+															categoryDisplay
+																? `<span class="badge bg-secondary mb-2">${categoryDisplay}</span>`
+																: ""
+														}
+                            <div class="mt-auto d-flex justify-content-between align-items-center">
+                                <span class="card-price">$${parseFloat(
+																	producto.price || 0
+																).toFixed(2)}</span>
+                                <a href="${productUrl}" class="btn btn-primary" target="_blank" rel="noopener noreferrer">Ver Producto</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+		productCardsContainer.appendChild(cardCol);
+	});
+}
+
+// Función para generar los controles de paginación
+function setupPagination(totalItems) {
+	paginationControls.innerHTML = "";
+	const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+	const createPageItem = (page, text, isDisabled = false, isActive = false) => {
+		const li = document.createElement("li");
+		li.classList.add("page-item");
+		if (isDisabled) li.classList.add("disabled");
+		if (isActive) li.classList.add("active");
+		li.innerHTML = `<a class="page-link" href="#" data-page="${page}">${text}</a>`;
+		return li;
+	};
+
+	// Botón "Anterior"
+	paginationControls.appendChild(
+		createPageItem(currentPage - 1, "&laquo;", currentPage === 1)
+	);
+
+	// Números de página
+	const maxPageButtons = 10;
+	let startPage = Math.max(1, currentPage - Math.floor(maxPageButtons / 2));
+	let endPage = Math.min(totalPages, startPage + maxPageButtons - 1);
+
+	if (endPage - startPage + 1 < maxPageButtons) {
+		startPage = Math.max(1, endPage - maxPageButtons + 1);
+	}
+
+	for (let i = startPage; i <= endPage; i++) {
+		paginationControls.appendChild(
+			createPageItem(i, i, false, i === currentPage)
+		);
+	}
+
+	// Botón "Siguiente"
+	paginationControls.appendChild(
+		createPageItem(currentPage + 1, "&raquo;", currentPage === totalPages)
+	);
+
+	// Añadir event listeners a los botones de paginación
+	paginationControls.querySelectorAll(".page-link").forEach((link) => {
+		link.addEventListener("click", (event) => {
+			event.preventDefault();
+			const newPage = parseInt(event.target.dataset.page);
+			if (
+				!isNaN(newPage) &&
+				newPage > 0 &&
+				newPage <= totalPages &&
+				newPage !== currentPage
+			) {
+				currentPage = newPage;
+				displayProducts(filteredProducts, currentPage); // Paginación sobre los productos filtrados
+				setupPagination(totalItems); // Regenerar controles
+			}
+		});
+	});
+}
+
+// Función para aplicar el filtro de categoría
+function applyCategoryFilter(category) {
+	currentCategoryFilter = category; // Actualiza el filtro actual
+	currentPage = 1; // Siempre vuelve a la primera página cuando se aplica un nuevo filtro
+
+	if (category === "Todas") {
+		filteredProducts = allProducts; // Si es "Todas", muestra todos los productos
+	} else {
+		// Filtra productos donde la categoría coincida (insensible a mayúsculas/minúsculas)
+		// y elimina espacios extra para una coincidencia más robusta
+		filteredProducts = allProducts.filter(
+			(p) =>
+				p.category && p.category.trim().toUpperCase() === category.toUpperCase()
+		);
+	}
+	displayProducts(filteredProducts, currentPage); // Muestra los productos filtrados
+	setupPagination(filteredProducts.length); // Reconfigura la paginación para los productos filtrados
+	updateActiveBadge(category); // Actualiza el estilo del badge activo
+}
+
+// Función para generar los badges de categoría y añadir listeners
+function generateCategoryBadges(categories) {
+	categoryBadgesContainer.innerHTML = ""; // Limpiar contenedor
+	// Añadir "Todas" como primera opción si no está ya
+	const uniqueCategories = new Set(["Todas", ...categories]);
+
+	uniqueCategories.forEach((categoryName) => {
+		const anchorElement = document.createElement("a");
+		anchorElement.href = "#";
+		anchorElement.classList.add("badge-category");
+		anchorElement.textContent = categoryName;
+		// Añadir clase 'active-filter' si es la categoría actualmente seleccionada
+		if (categoryName === currentCategoryFilter) {
+			anchorElement.classList.add("active-filter");
+		}
+
+		anchorElement.addEventListener("click", (event) => {
+			event.preventDefault();
+			applyCategoryFilter(categoryName);
+		});
+
+		categoryBadgesContainer.appendChild(anchorElement);
+	});
+}
+
+// Función para actualizar la clase 'active-filter' en los badges
+function updateActiveBadge(selectedCategory) {
+	const badges = categoryBadgesContainer.querySelectorAll(".badge-category");
+	badges.forEach((badge) => {
+		if (badge.textContent === selectedCategory) {
+			badge.classList.add("active-filter");
+		} else {
+			badge.classList.remove("active-filter");
+		}
+	});
+}
+
+// Tu función desencriptar
+function desencriptar(data) {
+	const key = CryptoJS.enc.Utf8.parse("12345678901234567890123456789012");
+	const iv = CryptoJS.enc.Utf8.parse("1234567890123456");
+
+	console.log(data);
+	const encrypted = data;
+
+	try {
+		const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+			iv: iv,
+			mode: CryptoJS.mode.CBC,
+			padding: CryptoJS.pad.Pkcs7,
+		});
+		const decryptedString = decrypted.toString(CryptoJS.enc.Utf8);
+		const result = JSON.parse(decryptedString);
+		console.log(result);
+		// Asegúrate de que 'result' sea el array de productos
+		// Si el JSON desencriptado es un array directamente, usa result.
+		// Si el JSON desencriptado es un objeto con una propiedad que contiene el array, ajústalo.
+		allProducts = JSON.parse(result); // O result.products si tu JSON tiene esa estructura
+		console.log(allProducts, typeof allProducts);
+		// Normalizar las categorías al cargar todos los productos
+		allProducts = allProducts.map((p) => ({
+			...p,
+			category: p.category ? p.category.trim() : "", // Limpiar espacios en blanco
+		}));
+
+		filteredProducts = allProducts; // Inicialmente, todos los productos están filtrados
+
+		// Obtener categorías únicas de todos los productos para generar los badges
+		const uniqueCategoriesFromProducts = [
+			...new Set(allProducts.map((p) => p.category).filter(Boolean)),
+		];
+		generateCategoryBadges(uniqueCategoriesFromProducts); // Genera los badges con las categorías encontradas
+
+		applyCategoryFilter(currentCategoryFilter); // Aplica el filtro inicial ("Todas")
+	} catch (error) {
+		console.error("Error al desencriptar o parsear el JSON:", error);
+		productCardsContainer.innerHTML =
+			'<div class="col-12"><p class="text-danger">Error al procesar los datos.</p></div>';
+	}
+}
+
+// Tu función getData
+function getData(buscarTexto) {
+	document.getElementById("loader").style.display = "block";
+	document.getElementById("search-input").disabled = true;
+	var settings = {
+		url: "https://script.google.com/macros/s/AKfycbziLnqsOy3sACUyBx9B1783dsbQRdddf_dmg0MEFEO8gMw4qD-ooNBtBo-kf7o9c5qy/exec",
+		type: "POST",
+		data: JSON.stringify({
+			texto: buscarTexto, // Esto probablemente debería ser dinámico para tu búsqueda
+		}),
+		// contentType: "application/json; charset=utf-8", // Asegúrate de enviar JSON correctamente
+		// dataType: "json", // Esperar una respuesta JSON
+	};
+
+	$.ajax(settings)
+		.done(function (response) {
+			console.log("Respuesta de la API:", response);
+			const data = JSON.parse(response);
+			// Asumiendo que response ya es un objeto JSON y contiene 'respuesta.respuesta'
+			if (data && data.respuesta && data.respuesta.respuesta) {
+				desencriptar(data.respuesta.respuesta);
+			} else {
+				console.error("Estructura de respuesta inesperada:", response);
+				productCardsContainer.innerHTML =
+					'<div class="col-12"><p class="text-danger">Estructura de datos inesperada de la API.</p></div>';
+			}
+			document.getElementById("loader").style.display = "none";
+			document.getElementById("search-input").disabled = false;
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			document.getElementById("loader").style.display = "none";
+			document.getElementById("search-input").disabled = false;
+			console.error(
+				"Error al obtener datos de la API:",
+				textStatus,
+				errorThrown
+			);
+			productCardsContainer.innerHTML =
+				'<div class="col-12"><p class="text-danger">No se pudieron cargar los datos de la API. Inténtalo de nuevo más tarde.</p></div>';
+		});
+}
+
+// Evento para el campo de búsqueda (al presionar Enter)
+searchInput.addEventListener("keyup", (event) => {
+	if (event.key === "Enter") {
+		console.log(searchInput.value);
+		getData(searchInput.value);
+		// applySearchFilter();
+		// // Opcional: Deseleccionar el badge de categoría activa al buscar
+		// currentCategoryFilter = "Todas";
+		// updateActiveBadge(currentCategoryFilter);
+	}
+});
+
+// Evento para el icono de limpiar búsqueda (la 'x')
+clearSearchIcon.addEventListener("click", () => {
+	searchInput.value = ""; // Limpia el campo
+	currentSearchTerm = ""; // Limpia el término de búsqueda en la variable
+	applyFilters(); // Re-aplica los filtros (sin término de búsqueda)
+});
